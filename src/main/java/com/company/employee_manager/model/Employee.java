a97361207ff5d16d99d74f7fca5f91bb3d7c8f67
Gabriel <blechdroide@gmail.com>
2023-10-02 10:01:14 +0200

    Employee model created

---
+++ src/main/java/com/company/employee_manager/model/Employee.java
@@ -0,0 +1,51 @@
+package com.company.employee_manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private long ID;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String mail;
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
