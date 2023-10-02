aa1f90441c5d9726a44d1c3afd6f230fe0ce3155
Gabriel <blechdroide@gmail.com>
2023-10-02 10:30:48 +0200

    Post/Get/Delete for Employees added + EmployeeRepository created

---
+++ src/main/java/com/company/employee_manager/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.company.employee_manager.controller;
+
+import com.company.employee_manager.model.Employee;
+import com.company.employee_manager.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("employee")
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @PostMapping
+    Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+    @GetMapping
+    List<Employee> findAllEmployee() {
+        return employeeRepository.findAll();
+    }
+    @DeleteMapping("/delete/{ID}")
+    void deleteEmployeeByID(@PathVariable long ID) {
+        employeeRepository.deleteById(ID);
+    }
+}
