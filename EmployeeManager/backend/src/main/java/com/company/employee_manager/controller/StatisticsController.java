756cd455fe6db26f348254b2dcc6f2bf1054b361
Gabriel <blechdroide@gmail.com>
2023-10-04 14:55:05 +0200

    added SQL query instead of streams

--- EmployeeManager/backend/src/main/java/com/company/employee_manager/controller/StatisticsController.java
+++ EmployeeManager/backend/src/main/java/com/company/employee_manager/controller/StatisticsController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,26 +26,17 @@ public class StatisticsController {
 
     @GetMapping
     public Map<String, Object> getStatistics() {
-        List<Employee> employees = employeeRepository.findAll();
         Map<String, Object> statistics = new HashMap<>();
 
-        var junior = employees.stream()
-                .max(Comparator.comparingInt(employee -> employee.getEntryDate().getYear()))
-                .stream().max(Comparator.comparingInt(employee -> employee.getEntryDate().getMonthValue()));
-
-
-        var senior = employees.stream()
-                .min(Comparator.comparingInt(employee -> employee.getEntryDate().getYear()))
-                .stream().min(Comparator.comparingInt(employee -> employee.getEntryDate().getMonthValue()));
-
-        var average = employees.stream()
-                .mapToInt(employee -> LocalDate.now().getYear() - employee.getEntryDate().getYear())
-                .average();
+        var junior = employeeRepository.getJunior();
+        var senior = employeeRepository.getSenior();
+        var averageTenure = employeeRepository.getAverageTenure();
 
         statistics.put("junior", junior);
         statistics.put("senior", senior);
-        statistics.put("average", average);
+        statistics.put("average", averageTenure);
 
         return statistics;
     }
+
 }
--- EmployeeManager/backend/src/main/java/com/company/employee_manager/repository/EmployeeRepository.java
+++ EmployeeManager/backend/src/main/java/com/company/employee_manager/repository/EmployeeRepository.java
@@ -2,10 +2,25 @@ package com.company.employee_manager.repository;
 
 import com.company.employee_manager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    @Query(value = "select *\n" +
+            "from employee e \n" +
+            "order by entry_date desc \n" +
+            "limit 1", nativeQuery = true)
+    Employee getJunior();
 
+
+    @Query(value = "select *\n" +
+            "from employee e \n" +
+            "order by entry_date asc \n" +
+            "limit 1", nativeQuery = true)
+    Employee getSenior();
+
+    @Query(value = "\n" +
+            "select AVG(extract (year from current_date)  - extract (year from entry_date)) as avergae_time \n" +
+            "from employee e  ", nativeQuery = true)
+    Object getAverageTenure();
 }
