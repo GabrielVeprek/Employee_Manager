94bdd8cb6e458826eededf8f2b396d9e381a6fd6
Gabriel <blechdroide@gmail.com>
2023-10-06 08:35:04 +0200

    added Icon to NavBar

--- EmployeeManager/frontend/src/layout/Navbar.jsx
+++ EmployeeManager/frontend/src/layout/Navbar.jsx
@@ -4,10 +4,14 @@ export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <span className="font-weight-bold fs-3 text-decoration-underline">Employee Manager</span>
-          </Link>
+        <div className="container-fluid mt-1">
+          <div>
+            <Link className="navbar-brand" to="/">
+          <img className="img-fluid mb-2"  src="src/images/Icon.png"  width="140" height="150"/>
+              <span className="font-weight-bold fs-3 text-decoration-underline m-3">Employee Manager</span>
+            </Link>
+          </div>
+
           <div>
             <Link
               className="btn btn-outline-light border-light shadow mx-2"
