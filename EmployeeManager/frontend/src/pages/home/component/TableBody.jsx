0ba25be6c1918919e6556919e643f1136a00fd9b
Gabriel <blechdroide@gmail.com>
2023-10-20 12:47:30 +0200

    TableBody update with exported components

--- EmployeeManager/frontend/src/pages/home/component/TableBody.jsx
+++ EmployeeManager/frontend/src/pages/home/component/TableBody.jsx
@@ -2,14 +2,17 @@ import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {employeeURL} from "../../../URLs/employeeURL.js";
-import {sliceID} from "../../../utils/ids.js"
-import {createHeader, getToken} from "../../../utils/getToken.js";
+import {createHeader, deleteAuthenticatedResult, getToken} from "../../../utils/getToken.js";
+import {ConfirmCancelButton} from "../../../buttonComponent/ConfirmCancelButton.jsx";
+import {ViewButton} from "./ViewButton.jsx";
+import {EditButton} from "./EditButton.jsx";
+import {TableBodyDate} from "./TableBodyDate.jsx";
 
 
 export function TableBody() {
     const [employee, setEmployee] = useState([]);
     const [isConfirmed, setConfirmed] = useState(false);
-    const [employeeToDelete, setEmployeeToDelete] = useState(null);
+    const [employeeToDelete, setEmployeeToDelete] = useState("");
 
     useEffect(() => {
         loadEmployee();
@@ -20,18 +23,17 @@ export function TableBody() {
         setEmployee(result.data);
     };
 
-    const deleteEmployee = async (id) => {
-        await axios.delete(`${employeeURL}/${id}`);
-        loadEmployee();
+    const deleteEmployee = async () => {
+        await deleteAuthenticatedResult(`${employeeURL}/${employeeToDelete}`)
+        await loadEmployee();
         setEmployeeToDelete();
         setConfirmed(false);
     };
 
     function handleDelete(employeeId) {
-        isConfirmed ?
-            deleteEmployee(employeeId)
-            :
-            setEmployeeToDelete(employeeId);
+        console.log(employeeId + " : employeeID")
+        setEmployeeToDelete(employeeId)
+        console.log(employeeId)
         setConfirmed(true);
     }
 
@@ -39,36 +41,23 @@ export function TableBody() {
         setConfirmed(false)
     }
 
-
     const fullContent =
         <>
             <tbody className="font-weight-light fs-5">
             {employee.map((employee) => {
                 const token = getToken();
-                const header = createHeader(token);
+                const jwt = createHeader(token);
                 return (
                     <tr key={employee.id}>
-                        <td className="text-primary">{employee.id.slice(0, 4)}</td>
-                        <td>{employee.firstName}</td>
-                        <td>{employee.lastName}</td>
-                        <td>{employee.mail}</td>
+                        <TableBodyDate employee={employee}/>
                         <td>
-                            <Link
-                                className="btn btn-primary mx-2 shadow"
-                                to={`/viewEmployee/${employee.id}`}>
-                                View
-                            </Link>
-                            <Link
-                                className="btn btn-outline-primary mx-2 shadow"
-                                to={`/editEmployee/${employee.id}`}>
-                                Edit
-                            </Link>
-                            {header?
+                            <ViewButton employee={employee}/>
+                            <EditButton employee={employee}/>
+                            {jwt ?
                                 <button
-                                className="btn btn-danger mx-2 shadow"
-                                onClick={() => handleDelete(employee.id)}>Delete
-                            </button>:""}
-
+                                    className="btn btn-danger mx-2 shadow"
+                                    onClick={() => handleDelete(employee.id)}>Delete
+                                </button> : ""}
                         </td>
                     </tr>
                 );
@@ -81,13 +70,9 @@ export function TableBody() {
             {isConfirmed ? (
                 <div className="container offset-md-4">
                     <div className="row text-center">
-                        <div className="col-md-7 offset-md-2 border rounded p-4 mt-2 shadow">
-                            <h4>Confirm Delete of {employee.map((employee) =>
-                                employee.firstName + " " + employee.lastName + ", ID: " + sliceID(employee.id))}</h4>
-                            <button
-                                className="btn btn-danger mx-2"
-                                onClick={() => handleDelete(employeeToDelete)}>Confirm Delete
-                            </button>
+                        <div className="col-md-7 offset-md-1 border rounded p-4 mt-2 shadow">
+                            <h4>Confirm Delete of {"Employee with ID : " + employeeToDelete}</h4>
+                            <ConfirmCancelButton deleteEmployee={deleteEmployee()}/>
                             <Link className="btn btn-secondary" to={"/"} onClick={handleCancel}>Cancel</Link>
                         </div>
                     </div>
