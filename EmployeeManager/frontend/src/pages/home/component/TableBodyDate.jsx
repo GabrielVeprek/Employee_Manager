f1d50e2c4f2a12d6f27910ad682bf92fe23e8fa4
Gabriel <blechdroide@gmail.com>
2023-10-20 12:46:12 +0200

    TableBodyDate component created

---
+++ EmployeeManager/frontend/src/pages/home/component/TableBodyDate.jsx
@@ -0,0 +1,12 @@
+import {sliceID} from "../../../utils/ids.js";
+
+export function TableBodyDate({employee}) {
+    return (<>
+
+            <td className="text-primary">{sliceID(employee.id)}</td>
+            <td>{employee.firstName}</td>
+            <td>{employee.lastName}</td>
+            <td>{employee.mail}</td>
+        </>
+    )
+}
