6b51038bcabfd2597362320c4af08fafd8735bf3
Gabriel <blechdroide@gmail.com>
2023-10-21 18:04:54 +0200

    created ButtonLogin.jsx component

---
+++ EmployeeManager/frontend/src/pages/login/component/ButtonLogin.jsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+export function ButtonLogin ({handleLogin}) {
+    return(
+        <Link
+            type="submit"
+            className="btn btn-outline-success mx-2"
+            onClick={handleLogin}
+            to={"/login"}
+        >
+            Login
+        </Link>
+    )
+}
