4bfe255e63f5f01dca1146e64c09b5dc63215aae
Gabriel <blechdroide@gmail.com>
2023-10-21 17:47:55 +0200

    updated LoginPage.jsx with external components

--- EmployeeManager/frontend/src/pages/login/LoginPage.jsx
+++ EmployeeManager/frontend/src/pages/login/LoginPage.jsx
@@ -1,9 +1,11 @@
 import {Buffer} from "buffer";
 import {useState} from "react";
-import {Link} from "react-router-dom";
 import {CancelButton} from "../../buttonComponent/CancelButton.jsx";
-import {ErrorLoginForm} from "./component/ErrorLoginForm.jsx";
-import {LoginForm} from "./component/LoginForm.jsx";
+import {FormLoginError} from "./component/FormLoginError.jsx";
+import {FormLogin} from "./component/FormLogin.jsx";
+import {ButtonLogin} from "./component/ButtonLogin.jsx";
+import {ButtonRegister} from "./component/ButtonRegister.jsx";
+import {DefaultMask} from "./component/DefaultMask.jsx";
 
 export function LoginPage({setIsLoggedIn}) {
     const BACKEND_LOGIN = 'http://localhost:8080/login';
@@ -34,59 +36,29 @@ export function LoginPage({setIsLoggedIn}) {
                     history.go(-3)
                 } else {
                     setWrongLogIn(true);
-                    throw new Error("message");
                 }
                 return response.text();
             })
-            .catch(error => {
-                console.log("ERROR MESSAGE: " + error);
-            });
     }
 
     const errorLoginForm =
-        <div className="container">
-            <div className="row text-center">
-                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                    <ErrorLoginForm user={user} handleUsernameChange={handleUsernameChange}
-                                    handlePasswordChange={handlePasswordChange}/>
-                    <Link
-                        type="submit"
-                        className="btn btn-outline-success mx-2"
-                        onClick={handleLogin}
-                        to={"/login"}
-                    >
-                        Login
-                    </Link>
-                    <Link to="/register" className="btn btn-outline-primary mx-2">
-                        Register
-                    </Link>
-                    <CancelButton/>
-                </div>
-            </div>
-        </div>
+        <DefaultMask>
+            <FormLoginError user={user} handleUsernameChange={handleUsernameChange}
+                            handlePasswordChange={handlePasswordChange}/>
+            <ButtonLogin handleLogin={handleLogin}/>
+            <ButtonRegister/>
+            <CancelButton/>
+        </DefaultMask>
 
 
     const loginForm =
-        <div className="container">
-            <div className="row text-center">
-                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                    <LoginForm user={user} handleUsernameChange={handleUsernameChange}
-                               handlePasswordChange={handlePasswordChange}/>
-                    <Link
-                        type="submit"
-                        className="btn btn-outline-success mx-2"
-                        onClick={handleLogin}
-                        to={"/login"}
-                    >
-                        Login
-                    </Link>
-                    <Link to="/register" className="btn btn-outline-primary mx-2">
-                        Register
-                    </Link>
-                    <CancelButton/>
-                </div>
-            </div>
-        </div>
+        <DefaultMask>
+            <FormLogin user={user} handleUsernameChange={handleUsernameChange}
+                       handlePasswordChange={handlePasswordChange}/>
+            <ButtonLogin handleLogin={handleLogin}/>
+            <ButtonRegister/>
+            <CancelButton/>
+        </DefaultMask>
 
     return (wrongLogIn ? errorLoginForm : loginForm)
 }
