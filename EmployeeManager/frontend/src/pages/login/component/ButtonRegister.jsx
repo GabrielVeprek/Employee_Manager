dce920f229317f045727fea40c370c8ea027bd4e
Gabriel <blechdroide@gmail.com>
2023-10-21 18:05:04 +0200

    created ButtonRegister.jsx component

---
+++ EmployeeManager/frontend/src/pages/login/component/ButtonRegister.jsx
@@ -0,0 +1,7 @@
+import {Link} from "react-router-dom";
+
+export function ButtonRegister() {
+    return (<Link to="/register" className="btn btn-outline-primary mx-2">
+        Register
+    </Link>)
+}
