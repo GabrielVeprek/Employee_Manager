f3f88fdc09006a6ecff86606e60e5db32b3f045c
Gabriel <blechdroide@gmail.com>
2023-10-23 14:52:47 +0200

    changed color from Update EmployeeBUtton

--- EmployeeManager/frontend/src/layout/buttonComponents/CreateEmployeeButton.jsx
+++ EmployeeManager/frontend/src/layout/buttonComponents/CreateEmployeeButton.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 export function CreateEmployeeButton() {
     return (
         <Link
-            className="btn btn-outline-light border-primary  bg-success shadow"
+            className="btn btn-outline-light border-light  bg-success shadow"
             to="/createEmployee"
             type="submit"
         >
