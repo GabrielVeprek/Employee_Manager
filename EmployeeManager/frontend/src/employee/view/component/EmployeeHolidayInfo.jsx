26d86eeba31d861594e841231e127f526681e787
Gabriel <blechdroide@gmail.com>
2023-10-24 12:16:03 +0200

    implemented new CardContainer

--- EmployeeManager/frontend/src/employee/view/component/EmployeeHolidayInfo.jsx
+++ EmployeeManager/frontend/src/employee/view/component/EmployeeHolidayInfo.jsx
@@ -1,13 +1,17 @@
 import {HolidayAmount} from "./HolidayAmount.jsx";
 import {HolidayNext} from "./HolidayNext.jsx";
 import {Overtime} from "./Overtime.jsx";
+import {CardContainer} from "./CardContainer.jsx";
 
 export function EmployeeHolidayInfo({employee}) {
     return (
-        <ul className="list-group list-group-flush">
-            <HolidayAmount amount={employee.holiday.amount}/>
-            <HolidayNext date={employee.holiday.nextHoliday}/>
-            <Overtime hours={employee.overtimeHour}/>
-        </ul>
+        <CardContainer>
+            Holiday Infos
+            <ul className="list-group list-group-flush">
+                <HolidayAmount amount={employee.holiday.amount}/>
+                <HolidayNext date={employee.holiday.nextHoliday}/>
+                <Overtime hours={employee.overtimeHour}/>
+            </ul>
+        </CardContainer>
     )
 }
