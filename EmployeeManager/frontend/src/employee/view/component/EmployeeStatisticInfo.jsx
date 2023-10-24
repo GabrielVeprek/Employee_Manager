f79b8e2cd54ce8fe209373652616b2087e38d719
Gabriel <blechdroide@gmail.com>
2023-10-24 12:16:13 +0200

    implemented new CardContainer

--- EmployeeManager/frontend/src/employee/view/component/EmployeeStatisticInfo.jsx
+++ EmployeeManager/frontend/src/employee/view/component/EmployeeStatisticInfo.jsx
@@ -2,14 +2,18 @@ import {EntryDate} from "./EntryDate.jsx";
 import {Position} from "./Position.jsx";
 import {Salary} from "./Salary.jsx";
 import {Task} from "./Task.jsx";
+import {CardContainer} from "./CardContainer.jsx";
 
 export function EmployeeStatisticInfo({employee}) {
     return (
-        <ul className="list-group list-group-flush">
-            <EntryDate entryDate={employee.entryDate}/>
-            <Position position={employee.position}/>
-            <Salary salary={employee.salary}/>
-            <Task task={employee.task}/>
-        </ul>
+        <CardContainer>
+            Personal Statistics
+            <ul className="list-group list-group-flush">
+                <EntryDate entryDate={employee.entryDate}/>
+                <Position position={employee.position}/>
+                <Salary salary={employee.salary}/>
+                <Task task={employee.task}/>
+            </ul>
+        </CardContainer>
     )
 }
