995ee3eb8fa58b92bc91d7ea702429b3150c35ba
Gabriel <blechdroide@gmail.com>
2023-10-24 11:09:25 +0200

    created new Component for employee.holiday.amount

---
+++ EmployeeManager/frontend/src/employee/view/component/HolidayAmount.jsx
@@ -0,0 +1,8 @@
+export  function HolidayAmount ({amount}) {
+    return (
+        <li className="list-group-item">
+            <b>Holidays left : </b>
+            {amount}
+        </li>
+    )
+}
