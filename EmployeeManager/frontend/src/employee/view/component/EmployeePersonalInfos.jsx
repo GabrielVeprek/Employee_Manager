4e0b514a5c1fa375cd3321137e433dd831710f74
Gabriel <blechdroide@gmail.com>
2023-10-24 12:16:09 +0200

    implemented new CardContainer

--- EmployeeManager/frontend/src/employee/view/component/EmployeePersonalInfos.jsx
+++ EmployeeManager/frontend/src/employee/view/component/EmployeePersonalInfos.jsx
@@ -1,15 +1,19 @@
 import {FirstName} from "./FirstName.jsx";
 import {LastName} from "./LastName.jsx";
 import {Mail} from "./Mail.jsx";
+import {CardContainer} from "./CardContainer.jsx";
 
 export function EmployeePersonalInfos({employee}) {
 
 
     return (
-        <ul className="list-group list-group-flush">
-            <FirstName firstName={employee.firstName}/>
-            <LastName lastName={employee.lastName}/>
-            <Mail mail={employee.mail}/>
-        </ul>
+     <CardContainer>
+                <h6 className="text-dark">Employee - {employee.id}</h6>
+                <ul className="list-group list-group-flush">
+                    <FirstName firstName={employee.firstName}/>
+                    <LastName lastName={employee.lastName}/>
+                    <Mail mail={employee.mail}/>
+                </ul>
+     </CardContainer>
     )
 }
