648189a02f51d421fec92d4053b812a4b0b54084
Gabriel <blechdroide@gmail.com>
2023-10-24 12:15:35 +0200

    added my-2 for the DefaultMask.jsx container

--- EmployeeManager/frontend/src/pages/login/component/DefaultMask.jsx
+++ EmployeeManager/frontend/src/pages/login/component/DefaultMask.jsx
@@ -1,6 +1,6 @@
 export function DefaultMask({ children }) {
     return (
-        <div className="container">
+        <div className="container my-2">
             <div className="row text-center">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                     {children}
