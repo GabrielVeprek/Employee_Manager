471623692587039f08f0e67d26d55630057b6b2d
Gabriel <blechdroide@gmail.com>
2023-10-25 15:38:23 +0200

    created new SQL Query for the Endpoint

--- EmployeeManager/backend/src/main/java/com/company/employee_manager/repository/EmployeeRepository.java
+++ EmployeeManager/backend/src/main/java/com/company/employee_manager/repository/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.company.employee_manager.repository;
 import com.company.employee_manager.model.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
@@ -13,7 +15,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
             "order by entry_date desc \n" +
             "limit 1", nativeQuery = true)
     Employee getJunior();
-
 
     @Query(value = "select *\n" +
             "from employee e \n" +
@@ -25,4 +26,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
             "select AVG(extract (year from current_date)  - extract (year from entry_date)) as avergae_time \n" +
             "from employee e  ", nativeQuery = true)
     Object getAverageTenure();
+
+    @Query(value = "SELECT * FROM employee e " +
+            "WHERE e.last_name ILIKE :input || '%' OR e.first_name ILIKE :input || '%' OR e.position ILIKE :input || '%'", nativeQuery = true)
+    List<Employee> searchByInput(@Param("input") String input);
 }
