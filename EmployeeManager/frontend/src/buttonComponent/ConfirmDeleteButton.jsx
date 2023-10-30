31751ffc31b0d6e5e03be355779781c64d161c17
Gabriel <blechdroide@gmail.com>
2023-10-30 14:39:21 +0100

    ConfirmDeleteButton.jsx component created

---
+++ EmployeeManager/frontend/src/buttonComponent/ConfirmDeleteButton.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+
+export function ConfirmDeleteButton({deleteEmployee}) {
+    return (
+        <Link
+            className="btn btn-danger mx-2"
+            to={"/"}
+            onClick={deleteEmployee}>Confirm Delete
+        </Link>
+    )
+}
