6b35106a5a326fe8b01f813f4ff4055eeaf30b4f
Gabriel <blechdroide@gmail.com>
2023-10-30 15:16:15 +0100

    Created ButtonDropdown.jsx component for the Navbar.jsx

---
+++ EmployeeManager/frontend/src/layout/components/ButtonDropdown.jsx
@@ -0,0 +1,31 @@
+import {ButtonStatistics} from "./ButtonStatistics.jsx";
+import {ButtonCreateEmpl} from "./ButtonCreateEmpl.jsx";
+import {ButtonLogout} from "./ButtonLogout.jsx";
+
+export function ButtonDropdown ({isLoggedIn}) {
+    return(
+        <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle btn bg-primary text-light"
+               id="navbarDropdown"
+               role="button"
+               data-bs-toggle="dropdown"
+               aria-haspopup="true"
+               aria-expanded="false">
+                More
+            </a>
+            <div className="dropdown-menu bg-white" aria-labelledby="navbarDropdown">
+                <div className="container modal-dialog-centered">
+                    <ol className="li my-1">
+                        <ButtonStatistics/>
+                    </ol>
+                    <ol className="li my-1 ">
+                        <ButtonCreateEmpl/>
+                    </ol>
+                    <ol className="li my-1">
+                        <ButtonLogout isLoggedIn={isLoggedIn}/>
+                    </ol>
+                </div>
+            </div>
+        </li>
+    )
+}
