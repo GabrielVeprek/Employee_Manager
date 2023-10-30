d9f894c16a77f5fa490e0c2fa540ce05c59a17db
Gabriel <blechdroide@gmail.com>
2023-10-30 14:37:23 +0100

    implemented external components

--- EmployeeManager/frontend/src/pages/home/component/TableBody.jsx
+++ EmployeeManager/frontend/src/pages/home/component/TableBody.jsx
@@ -1,19 +1,15 @@
-import {Link} from "react-router-dom";
 import {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import {employeeURL} from "../../../URLs/employeeURL.js";
-import {deleteAuthenticatedResult} from "../../../utils/getToken.js";
-import {ConfirmCancelButton} from "../../../buttonComponent/ConfirmCancelButton.jsx";
 import {ViewButton} from "./ViewButton.jsx";
 import {EditButton} from "./EditButton.jsx";
 import {TableBodyDate} from "./TableBodyDate.jsx";
 import {appContext} from "../../../App.jsx";
+import {DeleteButton} from "./DeleteButton.jsx";
 
 
 export function TableBody({isLoggedIn}) {
     const [employee, setEmployee] = useState([])
-    const [isConfirmed, setConfirmed] = useState(false)
-    const [employeeToDelete, setEmployeeToDelete] = useState("")
     const [{searchResult}] = useContext(appContext)
     const display = searchResult.length === 0 ? employee : searchResult
 
@@ -26,61 +22,21 @@ export function TableBody({isLoggedIn}) {
         setEmployee(result.data)
     }
 
-
-    const deleteEmployee = async () => {
-        await deleteAuthenticatedResult(`${employeeURL}/${employeeToDelete}`)
-        await loadEmployee()
-        setEmployeeToDelete()
-        setConfirmed(false)
-    }
-
-    function handleDelete(employeeId) {
-        setEmployeeToDelete(employeeId)
-        setConfirmed(true)
-    }
-
-    function handleCancel() {
-        setConfirmed(false)
-    }
-
-    const fullContent =
-        <>
-            <tbody className="font-weight-light fs-5">
-            {display.map((employee) => {
-                return (
-                    <tr key={employee.id}>
-                        <TableBodyDate employee={employee}/>
-                        {isLoggedIn ?
-                            <td>
-                                <ViewButton employee={employee}/>
-                                <EditButton employee={employee}/>
-
-                                <button
-                                    className="btn btn-danger mx-2 shadow"
-                                    onClick={() => handleDelete(employee.id)}>Delete
-                                </button>
-                            </td> : ""}
-                    </tr>
-                );
-            })}
-            </tbody>
-        </>;
-
     return (
-        <>
-            {isConfirmed ? (
-                <div className="container offset-md-4">
-                    <div className="row text-center">
-                        <div className="col-md-7 offset-md-1 border rounded p-4 mt-2 shadow">
-                            <h4>Confirm Delete of {"Employee with ID : " + employeeToDelete}</h4>
-                            <ConfirmCancelButton deleteEmployee={deleteEmployee()}/>
-                            <Link className="btn btn-secondary" to={"/"} onClick={handleCancel}>Cancel</Link>
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                fullContent
-            )}
-        </>
+        <tbody className="font-weight-light fs-5">
+        {display.map((employee) => {
+            return (
+                <tr key={employee.id}>
+                    <TableBodyDate employee={employee}/>
+                    {isLoggedIn ?
+                        <td>
+                            <ViewButton employee={employee}/>
+                            <EditButton employee={employee}/>
+                            <DeleteButton id={employee.id}/>
+                        </td> : ""}
+                </tr>
+            )
+        })}
+        </tbody>
     )
 }
