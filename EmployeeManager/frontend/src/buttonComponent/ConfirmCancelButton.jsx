018f23dfa2c67b24fc8e04a6b8483cfe2fda4e94
Gabriel <blechdroide@gmail.com>
2023-10-30 14:35:45 +0100

    created component for the Confirm Delete Page

--- EmployeeManager/frontend/src/buttonComponent/ConfirmCancelButton.jsx
+++
@@ -1,8 +0,0 @@
-export function ConfirmCancelButton ({deleteEmployee}) {
-    return(
-        <button
-            className="btn btn-danger mx-2"
-            onClick={deleteEmployee}>Confirm Delete
-        </button>
-    )
-}
