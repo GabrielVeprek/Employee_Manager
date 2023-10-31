d17ffcb9af549c40eaffad1811fed1a241872fc8
Gabriel <blechdroide@gmail.com>
2023-10-31 09:56:32 +0100

    SearchBar.jsx update with statement to handle no search input

--- EmployeeManager/frontend/src/layout/components/SearchBar.jsx
+++ EmployeeManager/frontend/src/layout/components/SearchBar.jsx
@@ -3,30 +3,28 @@ import {useContext, useState} from "react";
 import {appContext} from "../../App.jsx";
 
 export function SearchBar() {
-    const [inputValue, setInputValue] = useState()
+    const [inputValue, setInputValue] = useState("")
     const [, setAppState] = useContext(appContext)
 
-    const onInputChange = (event) => {
+    async function handleSearch(event) {
         setInputValue(event.target.value)
-    }
-
-    async function handleSearch() {
-        const response = await axios.get(`http://localhost:8080/employee/search/${inputValue}`)
-        setAppState((currenState) => ({
-            ...currenState, searchResult: response.data
-        }))
+            const response = await axios.get(event.target.value === "" ?`http://localhost:8080/employee` :`http://localhost:8080/employee/search/${event.target.value}`)
+            setAppState((currenState) => ({
+                ...currenState, searchResult: response.data
+            }))
     }
 
     return (
         <div className="d-flex align-items-center ml-auto">
-            <form className="d-flex" role="search" onSubmit={event => event.preventDefault()}>
+            <form className="d-flex" role="search" onChange={event => event.preventDefault()}>
                 <input className="form-control me-2"
+
                        aria-label="Search"
                        type="text"
-                       placeholder="Search"
+                       placeholder="Firstname or Lastname..."
                        name="inputValue"
                        value={inputValue ?? ""}
-                       onChange={onInputChange}
+                       onChange={(event) => handleSearch(event)}
                 ></input>
                 <button className="btn btn-outline-light" type="submit" onClick={handleSearch}>Search
                 </button>
