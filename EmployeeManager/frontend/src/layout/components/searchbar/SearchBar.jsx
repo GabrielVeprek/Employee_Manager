c4854e34c8b4a8b59577b5a7b0c6e2247fcd4174
Gabriel <blechdroide@gmail.com>
2023-11-02 15:23:27 +0100

    created new Error message if no User found + impl InputField component

--- EmployeeManager/frontend/src/layout/components/searchbar/SearchBar.jsx
+++ EmployeeManager/frontend/src/layout/components/searchbar/SearchBar.jsx
@@ -1,31 +1,35 @@
 import axios from "axios";
 import {useContext, useState} from "react";
-import {appContext} from "../../App.jsx";
+import {appContext} from "../../../App.jsx";
+import {InputField} from "./InputField.jsx";
 
 export function SearchBar() {
     const [inputValue, setInputValue] = useState("")
     const [, setAppState] = useContext(appContext)
+    const [noUserFound, setNoUserFound] = useState(false)
 
     async function handleSearch(event) {
         setInputValue(event.target.value)
+
         const response = await axios.get(event.target.value === "" ? `http://localhost:8080/employee` : `http://localhost:8080/employee/search/${event.target.value}`)
         setAppState((currenState) => ({
             ...currenState, searchResult: response.data
         }))
+        setNoUserFound(response.data.length === 0)
     }
+
 
     return (
         <div className="d-flex align-items-center ml-auto">
-            <form className="d-flex" role="search" onChange={event => event.preventDefault()}>
-                <input className="form-control me-2"
-                       aria-label="Search"
-                       type="text"
-                       placeholder="Firstname or Lastname..."
-                       name="inputValue"
-                       value={inputValue ?? ""}
-                       onChange={(event) => handleSearch(event)}
-                ></input>
-            </form>
+            <InputField inputValue={inputValue} handleSearch={handleSearch} noUserFound={noUserFound}/>
+            {noUserFound ? (
+                <div className="alert alert-danger bottom-0" role="alert">
+                    No employee found ! 🧐🧐
+                </div>
+            ) : (
+                ""
+            )}
         </div>
+
     )
 }
