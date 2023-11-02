15d21ce2359ddaeac917df35314e37dbe00b6dc6
Gabriel <blechdroide@gmail.com>
2023-11-02 21:32:36 +0100

    created new RegisterForm.jsx component for Register.jsx

---
+++ EmployeeManager/frontend/src/pages/register/component/RegisterForm.jsx
@@ -0,0 +1,16 @@
+import {UsernameInputField} from "./UsernameInputField.jsx";
+import {PasswordInputField} from "./PasswordInputField.jsx";
+
+export function RegisterForm({user, handleUsernameChange, handlePasswordChange}) {
+    return (
+        <form>
+            <UsernameInputField
+                user={user}
+                handleUsernameChange={handleUsernameChange}/>
+
+            <PasswordInputField
+                user={user}
+                handlePasswordChange={handlePasswordChange}/>
+        </form>
+    )
+}
