6d40b428623ee0d31cd31cc53e457859eb1415ee
Gabriel <blechdroide@gmail.com>
2023-11-02 21:19:19 +0100

    created cosnt URl for Register and HomePage

---
+++ EmployeeManager/frontend/src/URLs/EmployeeStatsURL.js
@@ -0,0 +1,5 @@
+export const employeeStatsURL = "http://localhost:8080/employeeStatistics"
+export const employeeLoginURL = "http://localhost:8080/login"
+export const employeeURL = 'http://localhost:8080/employee'
+export const employeeRegisterURL = 'http://localhost:8080/register'
+export const employeeHomeURL = 'http://localhost:5173'
--- EmployeeManager/frontend/src/URLs/EmployeeURLs.js
+++
@@ -1,3 +0,0 @@
-export const employeeURLs = "http://localhost:8080/employeeStatistics";
-export const employeeLoginURL = "http://localhost:8080/login";
-export const employeeURL = 'http://localhost:8080/employee';
