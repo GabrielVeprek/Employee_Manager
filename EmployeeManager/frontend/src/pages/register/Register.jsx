b55517975dc4564c9b22a20da212825fc9300ddd
Gabriel <blechdroide@gmail.com>
2023-11-02 21:33:12 +0100

    updated Register.jsx with external components

--- EmployeeManager/frontend/src/pages/register/Register.jsx
+++ EmployeeManager/frontend/src/pages/register/Register.jsx
@@ -3,6 +3,8 @@ import {CancelButton} from "../../buttonComponent/CancelButton.jsx"
 import {useState} from "react"
 import {employeeRegisterURL} from "../../URLs/EmployeeStatsURL.js"
 import axios from "axios"
+import {RegisterForm} from "./component/RegisterForm.jsx";
+import {DefaultMask} from "../login/component/DefaultMask.jsx";
 
 export function Register() {
 
@@ -32,45 +34,20 @@ export function Register() {
     }
 
     return (
-        <div className="container">
-            <div className="row text-center">
-                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                    <h2 className="text-center m-4">Register</h2>
-                    <form>
-                        <div className="mb-3">
-                            <label
-                                htmlFor="FirstName"
-                                className="form-label">
-                                Username
-                            </label>
-                            <input
-                                type={"text"}
-                                className="form-control"
-                                placeholder="User-Name"
-                                name="username"
-                                value={user.username}
-                                onChange={handleUsernameChange}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="FirstName" className="form-label">
-                                Password
-                            </label>
-                            <input
-                                type={"password"}
-                                className="form-control"
-                                placeholder="Password"
-                                name="password"
-                                value={user.password}
-                                onChange={handlePasswordChange}
-                            />
-                        </div>
-                    </form>
-                    <Link type="submit" to={"/login"} className="btn btn-outline-success mx-2" onClick={handleRegister}>Register
-                    </Link>
-                    <CancelButton/>
-                </div>
-            </div>
-        </div>
+        <DefaultMask>
+            <h2 className="text-center m-4">Register</h2>
+            <RegisterForm
+                user={user}
+                handleUsernameChange={handleUsernameChange}
+                handlePasswordChange={handlePasswordChange}/>
+            <Link
+                type="submit"
+                to={"/login"}
+                className="btn btn-outline-success mx-2"
+                onClick={handleRegister}>
+                Register
+            </Link>
+            <CancelButton/>
+        </DefaultMask>
     )
 }
