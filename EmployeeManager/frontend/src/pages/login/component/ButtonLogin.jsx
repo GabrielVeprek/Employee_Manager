09bb4f42c8338150ea9b3ae91ef4fb4364bacec5
Gabriel <blechdroide@gmail.com>
2023-11-04 19:09:24 +0100

    changed button type to default "button

--- EmployeeManager/frontend/src/pages/login/component/ButtonLogin.jsx
+++ EmployeeManager/frontend/src/pages/login/component/ButtonLogin.jsx
@@ -3,7 +3,6 @@ import {Link} from "react-router-dom"
 export function ButtonLogin ({handleLogin}) {
     return(
         <Link
-            type="submit"
             className="btn btn-outline-success mx-2"
             onClick={handleLogin}
             to={"/login"}>
--- EmployeeManager/frontend/src/pages/login/component/ButtonRegister.jsx
+++ EmployeeManager/frontend/src/pages/login/component/ButtonRegister.jsx
@@ -3,7 +3,6 @@ import {Link} from "react-router-dom"
 export function ButtonRegister() {
     return (
         <Link
-            type="submit"
             className="btn btn-outline-primary mx-2"
             to="/register">
             Register
