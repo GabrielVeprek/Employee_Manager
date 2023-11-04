2f8226564766618d53652e43ada5958dc1d1ce15
Gabriel <blechdroide@gmail.com>
2023-11-04 19:09:43 +0100

    clean up CancelButton.jsx file

--- EmployeeManager/frontend/src/buttonComponent/CancelButton.jsx
+++ EmployeeManager/frontend/src/buttonComponent/CancelButton.jsx
@@ -1,8 +1,10 @@
-import {Link} from "react-router-dom" 
+import {Link} from "react-router-dom"
 
 export function CancelButton() {
     return (
-        <Link className="btn btn-danger mx-2" to="/">
+        <Link
+            className="btn btn-danger mx-2"
+            to="/">
             Cancel
         </Link>
     )
