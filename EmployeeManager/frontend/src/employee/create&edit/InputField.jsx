b097414c79717ff72f144b428e4c87a76642340a
Gabriel <blechdroide@gmail.com>
2023-11-04 18:39:54 +0100

    created 3. Card Contaier for Holiday Infos

--- EmployeeManager/frontend/src/employee/create&edit/InputField.jsx
+++ EmployeeManager/frontend/src/employee/create&edit/InputField.jsx
@@ -1,20 +1,20 @@
-import {SubmitButton} from "../../buttonComponent/SubmitButton.jsx" 
-import {CancelButton} from "../../buttonComponent/CancelButton.jsx" 
-import {useEditInputField} from "./customeHook/useEditInputField.jsx" 
-import {FirstName} from "./component/FirstName.jsx" 
-import {LastName} from "./component/LastName.jsx" 
-import {Mail} from "./component/Mail.jsx" 
-import {EntryDate} from "./component/EntryDate.jsx" 
-import {CardContainer} from "../view/component/CardContainer.jsx" 
-import {Position} from "./component/Position.jsx" 
-import {Salary} from "./component/Salary.jsx" 
-import {Task} from "./component/Task.jsx" 
-import {Holiday} from "./component/Holiday.jsx" 
-import {Overtime} from "./component/Overtime.jsx" 
-import {HolidayDate} from "./component/HolidayDate.jsx" 
+import {SubmitButton} from "../../buttonComponent/SubmitButton.jsx"
+import {CancelButton} from "../../buttonComponent/CancelButton.jsx"
+import {useEditInputField} from "./customeHook/useEditInputField.jsx"
+import {FirstName} from "./component/FirstName.jsx"
+import {LastName} from "./component/LastName.jsx"
+import {Mail} from "./component/Mail.jsx"
+import {EntryDate} from "./component/EntryDate.jsx"
+import {CardContainer} from "../view/component/CardContainer.jsx"
+import {Position} from "./component/Position.jsx"
+import {Salary} from "./component/Salary.jsx"
+import {Task} from "./component/Task.jsx"
+import {Holiday} from "./component/Holiday.jsx"
+import {Overtime} from "./component/Overtime.jsx"
+import {HolidayDate} from "./component/HolidayDate.jsx"
 
 export function InputField({id}) {
-    const {onSubmit, onInputChange, employee} = useEditInputField(id) 
+    const {onSubmit, onInputChange, employee} = useEditInputField(id)
 
     return (
         (
@@ -31,6 +31,9 @@ export function InputField({id}) {
                     <Position position={employee.position} onInputChange={onInputChange}/>
                     <Salary salary={employee.salary} onInputChange={onInputChange}/>
                     <Task task={employee.task} onInputChange={onInputChange}/>
+                </CardContainer>
+                <CardContainer>
+                    <h6 className="text-primary my-1">Holiday Infos</h6>
                     <Holiday amount={employee.holiday.amount} onInputChange={onInputChange}/>
                     <HolidayDate date={employee.holiday.nextHoliday} onInputChange={onInputChange}/>
                     <Overtime overtime={employee.overtimeHour} onInputChange={onInputChange}/>
