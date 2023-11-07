58aee05da87395d0eb827b4ade382249579981c1
Gabriel <blechdroide@gmail.com>
2023-11-07 11:13:46 +0100

    impl new dropdown menu for the Positions

--- EmployeeManager/frontend/src/employee/create&edit/component/Position.jsx
+++ EmployeeManager/frontend/src/employee/create&edit/component/Position.jsx
@@ -1,18 +1,42 @@
-export function Position ({position, onInputChange}) {
+import {useEffect, useState} from 'react';
+import {employeePositionsURL} from "../../../URLs/EmployeeStatsURL.js";
+import {getAuthenticatedResult} from "../../../utils/getToken.js";
+
+export function Position({onInputChange}) {
+    const [positions, setPositions] = useState([])
+    const [selectedPosition, setSelectedPosition] = useState('')
+
+    useEffect(() => {
+        const fetchPositions = async () => {
+            const response = await getAuthenticatedResult(employeePositionsURL)
+            setPositions(response.data)
+        }
+        fetchPositions()
+    }, [])
+
+    const handlePositionChange = (event) => {
+        setSelectedPosition(event.target.value)
+        onInputChange(event)
+    }
+
     return (
         <div className="mb-3">
             <label className="form-label text-dark">
                 Position
             </label>
-            <input
-                type={"text"}
+            <select
                 className="form-control"
-                placeholder="Position"
                 name="position"
-                value={position ?? ""}
-                onChange={(event) => onInputChange(event)}
-            />
+                value={selectedPosition}
+                onChange={handlePositionChange}
+            >
+                <option value="">Select a Position</option>
+                {positions.map((position) => (
+                    <option key={position} value={position}>
+                        {position}
+                    </option>
+                ))}
+            </select>
         </div>
-    )
-
+    );
 }
