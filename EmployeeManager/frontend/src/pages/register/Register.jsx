8b3c02f20709a4b8d39d21c3176a34ea5b40ad1b
Gabriel <blechdroide@gmail.com>
2023-11-07 18:09:29 +0100

    implemented new method to check if the register input is valid. + error message if not valid

--- EmployeeManager/frontend/src/pages/register/Register.jsx
+++ EmployeeManager/frontend/src/pages/register/Register.jsx
@@ -1,52 +1,83 @@
-import {Link} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 import {CancelButton} from "../../buttonComponent/CancelButton.jsx"
 import {useState} from "react"
 import {employeeRegisterURL} from "../../URLs/EmployeeStatsURL.js"
-import axios from "axios"
 import {RegisterForm} from "./component/RegisterForm.jsx";
 import {DefaultMask} from "../../component/DefaultMask.jsx";
 
-
 export function Register() {
+
+    const navigate = useNavigate();
+    const [isInputInvalid, setIsInvalidInput] = useState(null)
+    const [message, setMessage] = useState("Register")
 
     const [user, setUser] = useState({
         username: "",
         password: "",
-    })
+        authorities: ["USER"]
+    });
 
-    function handleUsernameChange(event) {
-        setUser({...user, username: event.target.value})
-    }
+    const handleUsernameChange = (event) => {
+        setUser({...user, username: event.target.value});
+    };
 
-    function handlePasswordChange(event) {
-        setUser({...user, password: event.target.value})
-    }
+    const handlePasswordChange = (event) => {
+        setUser({...user, password: event.target.value});
+    };
+
 
     function handleRegister() {
-        const user1 = {
-            username: user.username,
-            password: user.password,
-            authorities: ["USER"]
+
+        if (user.username.trim().length < 1) {
+            setIsInvalidInput(true)
+            setMessage("Username cannot be empty")
+        } else {
+            setIsInvalidInput(null)
+        }
+        if (user.password.trim().length < 1) {
+            setIsInvalidInput(true)
+            setMessage("Password cannot be empty")
+        }
+        if (user.password.trim().length < 8) {
+            setIsInvalidInput(true)
+            setMessage("Password must have at least 8 digits")
         }
 
-        axios.post(employeeRegisterURL, user1, {
-            headers: {'Content-Type': 'application/json'}
-        })
+        if (user.password.trim().length > 7 && user.username.trim().length > 1) {
+            fetch(employeeRegisterURL, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(user),
+            })
+                .then(response => {
+                    if (response.status === 400) {
+                        setIsInvalidInput(true)
+                        setMessage("Username already taken")
+                    }
+                    if (response.status === 201) {
+                        navigate("/login")
+                    }
+                })
+        }
     }
+
 
     return (
         <DefaultMask>
-            <h2 className="text-center m-4">Register</h2>
+            <h2 className={isInputInvalid ? "text-center m-4 text-danger" : "text-center m-4"}>
+                {message}
+            </h2>
             <RegisterForm
                 user={user}
                 handleUsernameChange={handleUsernameChange}
-                handlePasswordChange={handlePasswordChange}/>
-            <Link
-                to={"/login"}
+                handlePasswordChange={handlePasswordChange}
+                setIsSomethingWrong={isInputInvalid}
+            />
+            <button
                 className="btn btn-outline-success mx-2"
                 onClick={handleRegister}>
                 Register
-            </Link>
+            </button>
             <CancelButton/>
         </DefaultMask>
     )
