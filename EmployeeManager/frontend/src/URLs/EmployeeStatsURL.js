793fe4d85115c4b516d8c405afb0ad4e34ade6b7
Gabriel <blechdroide@gmail.com>
2023-11-07 11:10:54 +0100

    created new URL const for positions

--- EmployeeManager/frontend/src/URLs/EmployeeStatsURL.js
+++ EmployeeManager/frontend/src/URLs/EmployeeStatsURL.js
@@ -2,4 +2,6 @@ export const employeeStatsURL = "http://localhost:8080/employeeStatistics"
 export const employeeLoginURL = "http://localhost:8080/login"
 export const employeeURL = 'http://localhost:8080/employee'
 export const employeeRegisterURL = 'http://localhost:8080/register'
+
+export const employeePositionsURL= 'http://localhost:8080/employee/position'
 export const employeeHomeURL = 'http://localhost:5173'
