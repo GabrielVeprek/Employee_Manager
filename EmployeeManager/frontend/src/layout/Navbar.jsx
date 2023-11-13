00f7b27798f42733aefed5a64a96ff6809e41ead
Gabriel <blechdroide@gmail.com>
2023-11-13 12:43:51 +0100

    changed style from container to mx-3

--- EmployeeManager/frontend/src/layout/Navbar.jsx
+++ EmployeeManager/frontend/src/layout/Navbar.jsx
@@ -1,34 +1,44 @@
-import {HomePageImage} from "./components/HomePageImage.jsx" 
-import {HomePageTitle} from "./components/HomePageTitle.jsx" 
-import {ButtonLogin} from "./components/ButtonLogin.jsx" 
-import {SearchBar} from "./components/searchbar/SearchBar.jsx" 
-import {ButtonDropdown} from "./components/ButtonDropdown.jsx" 
-import {ImageSearchBar} from "./components/searchbar/ImageSearchBar.jsx" 
+import {HomePageImage} from "./components/HomePageImage.jsx"
+import {HomePageTitle} from "./components/HomePageTitle.jsx"
+import {ButtonLogin} from "./components/ButtonLogin.jsx"
+import {SearchBar} from "./components/searchbar/SearchBar.jsx"
+import {ButtonDropdown} from "./components/ButtonDropdown.jsx"
+import {ImageSearchBar} from "./components/searchbar/ImageSearchBar.jsx"
 
 export default function Navbar({isLoggedIn}) {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid mt-1">
-                    <HomePageImage/>
-                    <HomePageTitle/>
+                    <div>
+                        <HomePageImage/>
+                        <HomePageTitle/>
+                    </div>
                 </div>
-                <div className="container offset-md-4">
-                    <div className="d-flex align-items-center">
+                <div className="row mx-3">
+                    <div className="d-flex align-items-center mx-3">
                         <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                {isLoggedIn ?
-                                    <ButtonDropdown isLoggedIn={isLoggedIn}/>
-                                    :
-                                    <ButtonLogin/>
-                                }
-                            </ul>
+                            <div className="row">
+                                <ul className="navbar-nav">
+                                    {isLoggedIn ?
+                                        <ButtonDropdown isLoggedIn={isLoggedIn}/>
+                                        :
+                                        <ButtonLogin/>
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <ImageSearchBar/>
+                        </div>
+                        <div className="col-md-8">
+                            <SearchBar/>
                         </div>
                     </div>
-                    <ImageSearchBar/>
-                    <SearchBar/>
                 </div>
             </nav>
         </div>
+
+
     )
 }
