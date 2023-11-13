95cd20f310871289843eb7d80807afab4b01bb61
Gabriel <blechdroide@gmail.com>
2023-11-13 12:43:10 +0100

    styled navbar dropdown button to style: left -100%

--- EmployeeManager/frontend/src/layout/components/ButtonDropdown.jsx
+++ EmployeeManager/frontend/src/layout/components/ButtonDropdown.jsx
@@ -1,31 +1,40 @@
-import {ButtonStatistics} from "./ButtonStatistics.jsx" 
-import {ButtonCreateEmpl} from "./ButtonCreateEmpl.jsx" 
-import {ButtonLogout} from "./ButtonLogout.jsx" 
+import { ButtonStatistics } from "./ButtonStatistics.jsx";
+import { ButtonCreateEmpl } from "./ButtonCreateEmpl.jsx";
+import { ButtonLogout } from "./ButtonLogout.jsx";
 
-export function ButtonDropdown ({isLoggedIn}) {
-    return(
+export function ButtonDropdown({ isLoggedIn }) {
+    return (
         <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle btn bg-primary text-light"
-               id="navbarDropdown"
-               role="button"
-               data-bs-toggle="dropdown"
-               aria-haspopup="true"
-               aria-expanded="false">
+            <a
+                className="nav-link dropdown-toggle btn bg-primary text-light"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+            >
                 More
             </a>
-            <div className="dropdown-menu bg-white" aria-labelledby="navbarDropdown">
+            <div
+                className="dropdown-menu bg-white"
+                aria-labelledby="navbarDropdown"
+                data-bs-popper="static"
+                style={{
+                    left: '-100%',
+                }}
+            >
                 <div className="container modal-dialog-centered">
                     <ol className="li my-1">
-                        <ButtonStatistics/>
-                    </ol>
-                    <ol className="li my-1 ">
-                        <ButtonCreateEmpl/>
+                        <ButtonStatistics />
                     </ol>
                     <ol className="li my-1">
-                        <ButtonLogout isLoggedIn={isLoggedIn}/>
+                        <ButtonCreateEmpl />
+                    </ol>
+                    <ol className="li my-1">
+                        <ButtonLogout isLoggedIn={isLoggedIn} />
                     </ol>
                 </div>
             </div>
         </li>
-    )
+    );
 }
